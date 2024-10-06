79832c7d440d550d1d6a7be87be5fa5bfdc0a4ed
argentumdemens <argentumdemens@gmail.com>
2024-10-06 19:10:15 -0400

    change folder structure and add terraform

---
+++ terraform/main.tf
@@ -0,0 +1,48 @@
+provider "google" {
+  project = var.project_id
+  region  = var.region
+}
+
+variable "project_id" {
+  description = "ID of the project where resources will go."
+  type        = string
+}
+
+variable "region" {
+  description = "Region of the Cloud Run service."
+  type        = string
+}
+
+terraform {
+  backend "gcs" {
+    bucket = "chatteroo-terraform-state"
+    prefix = "terraform/state"
+  }
+}
+
+resource "google_storage_bucket" "terraform_state_bucket" {
+  name          = "chatteroo-terraform-state"
+  location      = var.region
+  force_destroy = true
+  storage_class = "STANDARD"
+}
+
+resource "google_cloud_run_service" "chatteroo" {
+  name     = "chatteroo"
+  location = var.region
+
+  template {
+    spec {
+      containers {
+        image = "${var.region}-docker.pkg.dev/${var.project_id}/chatteroo-docker/chatapp:latest"
+
+        
+      }
+    }
+  }
+
+  traffic {
+    percent        = 100
+    latest_revision = true
+  }
+}
